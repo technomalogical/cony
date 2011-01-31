38698cf3b9aa0a9c60f8097db4527e6fc2e8513b
Alexander Artemenko <svetlyak.40wt@gmail.com>
2011-01-31 11:15:20 +0300

    More complex example, which uses API to fetch word translations from the internet.

--- example_commands.py
+++ example_commands.py
@@ -1,4 +1,23 @@
+# -*- coding: utf-8 -*-
+import os.path
+import urllib
 from bottle import redirect
+
+TEMPLATES = dict(
+    save_word = """
+<p>Translation "{{ word }}" was saved to ~/.words</p>
+%rebase layout title='Translation saved'
+""",
+    add_word = """
+<ul>
+    %for v in variants:
+        <li><a href="/?s=save_word+{{ v['en'].replace(' ', '+') }}%3B+{{ v['ru'].replace(' ', '+').replace(',', '%2C') }}">{{ v['en'] }}</a> — {{ v['ru'] }}</li>
+    %end
+</ul>
+%rebase layout title='Word translation'
+""",
+)
+
 
 def cmd_fl(term):
     """Search among Flickr photos under Creative Commons license."""
@@ -21,3 +40,35 @@ def cmd_tr(term):
 
 def cmd_pep(term):
     redirect('http://www.python.org/dev/peps/pep-%0.4d/' % int(term))
+
+def cmd_save_word(term):
+    """Saves word and it's translation into the ~/.words
+
+    This file could be used to import words into
+    the FlashCards ToGo.
+    """
+    with open(os.path.expanduser('~/.words'), 'a+') as f:
+        f.write(term)
+        f.write('\n')
+    return dict(template='save_word', word = term)
+
+def cmd_wo(term):
+    """Searches word translations at the http://slovari.yandex.ru.
+
+    This command requires `simplejson` module to be installed.
+    """
+    import simplejson
+    variants = []
+
+    for i in reversed(range((len(term) + 1) / 2, len(term) + 1)):
+        url = 'http://suggest-slovari.yandex.ru/suggest-lingvo?v=2&lang=en&part=%s' % term[:i]
+        data = urllib.urlopen(url).read()
+        data = simplejson.loads(data)
+        if data[0]:
+            for trans, link in zip(*data[1:]):
+                en, ru = trans.split(' - ', 1)
+                variants.append(dict(en=en, ru=ru, link=link))
+            if len(variants) > 5:
+                break
+
+    return dict(template='add_word', variants = variants)
