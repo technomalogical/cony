f3425564b26e8d524fb0c5c875da3f5ea49e82ca
Alexander Artemenko <svetlyak.40wt@gmail.com>
2011-02-03 02:36:20 +0300

    Version 0.2.0. There is incompatible change in config filename.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cony",
-    version="0.1.0",
+    version="0.2.0",
     description=(
         '"Cony" is a tool to write smart bookmarks in '
         'python and to share them across all your browsers and with a '
